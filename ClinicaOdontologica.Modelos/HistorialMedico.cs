110dbec88706c4c931e15c79ac9553d6062790e1
davidpp19 <kevdavidpena.06@gmail.com>
2026-09-22 21:50:34 -0500

    Creacion de tabla debil, con una llave foranea

---
+++ ClinicaOdontologica.Modelos/HistorialMedico.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaOdontologica.Modelos
+{
+    [Table("historialesmedicos")]
+    public class HistorialMedico
+    {
+        [Key]
+        [Column("id_historial", TypeName = "Serial")]
+        public int IdHistorialMedico { get; set; }
+
+        [Column("alergias")]
+        [MaxLength(200)]
+        public string alergia { get; set; }
+
+        [Column("enfermedades_previas")]
+        [MaxLength(200)]
+        public string enfermedadesPrevias { get; set; }
+
+        [Column("tipo_sangre")]
+        [MaxLength(4)]
+        public string tipoSangre { get; set; }
+
+        [ForeignKey("paciente")]
+        [Column("id_paciente")]
+        public int IdPaciente { get; set; }
+
+        //Objetos de navegacion
+        public Paciente? paciente { get; set; }
+
+    }
+
+}
